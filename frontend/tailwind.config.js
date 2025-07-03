3abc77d4bb53b222dda74bd6b3fbc3bc49ecdf5e
safat36 <alvibabu456@gmail.com>
2025-07-03 20:29:03 +0600

    Create tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#a8e6cf',
+      },
+    },
+  },
+  plugins: [],
+}
